75fd11130adebd88a919554b7e41095c1ac36238
Dimarfin <d.afanasenkov@gmail.com>
2022-08-30 11:31:19 +0200

    project description

--- main_streamlit_bak3.py
+++ main_streamlit_bak3.py
@@ -58,7 +58,7 @@ def only_high_contours(img, hight):
 #     cv2.fillPoly(mask, cnts, [255,255,255])
 #     mask = 255 - mask
 #     result = cv2.bitwise_or(img, mask)
-#     return cv2.erode(result, (3,3),iterations = 2)
+#     return result
 
 def img_preprocessor01(img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -95,7 +95,7 @@ def txt_postproc01(text):
 
 def imageInput(device, src):
     
-    if src == 'Upload your own image.':
+    if src == 'Upload your own data.':
         image_file = st.file_uploader("Upload An Image", type=['png', 'jpeg', 'jpg'])
         col1, col2 = st.columns(2)
         if image_file is not None:
@@ -117,14 +117,13 @@ def imageInput(device, src):
 
             #call Model prediction--
             #model = torch.hub.load('ultralytics/yolov5', 'custom', path=cfg_model_path, force_reload=True) 
-            model = torch.hub.load('.',
+            model = torch.hub.load('Dimarfin/License_plate_recognition',
                                    #'D:\Dima\DataScience\Projects\Cars_plate_recognition\Code\Yolov5\yolov5', 
                                    'custom', 
-                                   source='local', 
+                                   #source='local', 
                                    path=cfg_model_path, 
                                    force_reload=True, 
-                                   #device='cpu'
-                                   ) 
+                                   device='cpu') 
             #model.cuda() if device == 'cuda' else model.cpu()
             pred = model(imgpath)
             pred.render()  # render bbox in image
@@ -153,12 +152,13 @@ def imageInput(device, src):
                     tb.subheader(text)
                 shutil.rmtree('runs/detect/exp/')
                 
-    elif src == 'Choose an image from test set.': 
+    elif src == 'From test set.': 
         # Image selector slider
         imgpath = glob.glob('data/images/*')
         imgsel = st.slider('Select an image from the test set.', min_value=1, max_value=len(imgpath), step=1) 
         image_file = imgpath[imgsel-1]
         submit = st.button("Detect!")
+        #st.write(image_file)
         
         colA,colB = st.columns(2)
         with colB:
@@ -172,14 +172,13 @@ def imageInput(device, src):
             if image_file is not None and submit:
                 #call Model prediction--
                 #model = torch.hub.load('ultralytics/yolov5', 'custom', path=cfg_model_path, force_reload=True) 
-                model = torch.hub.load('.',
-                                       #'D:\Dima\DataScience\Projects\Cars_plate_recognition\Code\Yolov5\yolov5', 
-                                       'custom', 
-                                       source='local', 
-                                       path=cfg_model_path, 
-                                       force_reload=True, 
-                                       #device='cpu'
-                                       ) 
+                model = torch.hub.load('Dimarfin/License_plate_recognition',
+                                      #'D:\Dima\DataScience\Projects\Cars_plate_recognition\Code\Yolov5\yolov5', 
+                                      'custom', 
+                                      #source='local', 
+                                      path=cfg_model_path, 
+                                      force_reload=True, 
+                                      device='cpu') 
                 pred = model(image_file)
                 pred.render()  # render bbox in image
                 for im in pred.imgs:
@@ -203,22 +202,6 @@ def imageInput(device, src):
                     ta.write("Licence plate(s) text: ")
                     tb.subheader(text)
                 shutil.rmtree('runs/detect/exp/')
-    elif src == 'Show project description.':
-        st.header('Model description')
-        st.write('This model is the final project of the Data Science Boot Camp at the WBS coding school, Berlin. The source code can be found on GitHub [https://github.com/Dimarfin/License_plate_recognition](https://github.com/Dimarfin/License_plate_recognition)')
-        st.subheader('Model implementation')
-        st.write('This model implements detection the car license plate detection and recognition of the text on it. It includes steps several depicted in the figure below:')
-        fig01 = Image.open('docs/fig01.png')
-        st.image(fig01, caption='Schematic representation of the model pipeline')            
-        st.subheader('Object detection step')
-        st.markdown('The detection step is performed using [YOLOv5](https://github.com/ultralytics/yolov5) – a state of the art object detection system. It is based on a constitutional neuronal network and build using PyTorch library. YOLOv5 is supplied pertained on MS COCO dataset which speeds up the training step for a custom object. One of the review of the system can be found at the following [link](https://towardsdatascience.com/yolo-v5-is-here-b668ce2a4908). While there are several sizes of the YOLOv5 in this work YOLOv5s was utilized. For detection of the car license plates it was trained on a [data set from Kaggle](https://www.kaggle.com/datasets/andrewmvd/car-plate-detection). The model reached outstanding performance after 20 epochs of training. Some of the performance metrics measured on the validation subset are presented in the figure below.'
-                    , unsafe_allow_html=True)
-        fig02 = Image.open('docs/fig02.png')
-        st.image(fig02, caption='Model performance')  
-        st.subheader('Image processing step')
-        st.write('Optical character recognition can be improved if images of the detected licence plates are preprocessed to remove noise and unwanted feature which can be misrecognized as a character. This preprocessing step includes several operation such as converting image to a binary form, contour detection, erosion, dilation, etc. After that, prepared image is supplied to the OCR engine.')            
-        st.subheader('Optical character recognition')
-        st.write('To recognized text on the license plate tesseract [Tesseract Open Source OCR Engine](https://github.com/tesseract-ocr/tesseract) and a python library [pytesseract](https://pypi.org/project/pytesseract/) was used. The result of OCR is automatically analysed and, if necessary, other image preprocessing parameters or other parameters of the OCR engine ware used')
 
 
 
@@ -245,15 +228,12 @@ def videoInput(device, src):
 
 
 def main():
-    #pytesseract.pytesseract.tesseract_cmd = r'C:\Users\kater\AppData\Local\Tesseract-OCR\tesseract.exe'
     # -- Sidebar
+    #pytesseract.pytesseract.tesseract_cmd = r'C:\Users\kater\AppData\Local\Tesseract-OCR\tesseract.exe'
     st.sidebar.title('⚙️Options')
-    datasrc = st.sidebar.radio("", 
-                               ['Choose an image from test set.', 
-                                'Upload your own image.',
-                                'Show project description.'])
-         
-    st.sidebar.write('[GitHub link](https://github.com/Dimarfin/License_plate_recognition)')
+    datasrc = st.sidebar.radio("Select input source.", ['From test set.', 'Upload your own data.'])
+    
+        
     option = "Image" 
     deviceoption = 'cpu'
     #option = st.sidebar.radio("Select input type.", ['Image', 'Video'], disabled = True, index=0)
@@ -261,13 +241,11 @@ def main():
     #    deviceoption = st.sidebar.radio("Select compute Device.", ['cpu', 'cuda'], disabled = False, index=1)
     #else:
     #    deviceoption = st.sidebar.radio("Select compute Device.", ['cpu', 'cuda'], disabled = True, index=0)
-    #
     # -- End of Sidebar
     
     
     st.header('🚘 Car license plate recognition system')
     st.subheader('👈🏽 Select options from the left-hand menu bar')
-    st.markdown("""---""")
     #st.sidebar.markdown("https://github.com/thepbordin/Obstacle-Detection-for-Blind-people-Deployment")
     if option == "Image":    
         imageInput(deviceoption, datasrc)
