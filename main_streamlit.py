a82547d886e027e78e11ad4995b435d0033a9d0c
Dimarfin <d.afanasenkov@gmail.com>
2022-09-07 13:23:14 +0200

    edit model description

--- main_streamlit.py
+++ main_streamlit.py
@@ -179,14 +179,16 @@ def imageInput(device, src):
         st.write('This model is the final project of the Data Science Boot Camp at the WBS coding school, Berlin. The source code can be found on GitHub [https://github.com/Dimarfin/License_plate_recognition](https://github.com/Dimarfin/License_plate_recognition)')
         st.markdown('''
                     ### Model implementation
-                    This model implements detection the car license plate detection and recognition of the text on it.
-                    It includes two main steps several steps depicted in the figure below:
+                    This model implements detection the car license plate and recognition of 
+                    the text on it. It includes several steps depicted in the figure below. 
+                    The first two (training and validation) were performed only once while
+                    the following steps are repeated each time a new image is supplied to the model.
                     ''')
         fig01 = Image.open('docs/fig01.png')
-        st.image(fig01, caption='Schematic representation of the model pipeline')
+        st.image(fig01, width = 400, caption='Schematic representation of the model pipeline')
         st.markdown('''
                     ### Object detection step
-                    The detection step is performed using [YOLOv5](https://github.com/ultralytics/yolov5) – a state of the art object detection system. It is based on a constitutional neuronal network and build using PyTorch library. YOLOv5 is supplied pertained on MS COCO dataset which speeds up the training step for a custom object. One of the review of the system can be found at the following [link](https://towardsdatascience.com/yolo-v5-is-here-b668ce2a4908). 
+                    The detection step is performed using [YOLOv5](https://github.com/ultralytics/yolov5) – a state of the art object detection system. It is based on a constitutional neuronal network and build using PyTorch library. YOLOv5 is supplied pertained on MS COCO dataset which speeds up the training step for a custom object type. One of the review of the YOLO system can be found at the following [link](https://towardsdatascience.com/yolo-v5-is-here-b668ce2a4908). 
                     While there are several sizes of the YOLOv5 in this work YOLOv5s was utilized. For detection of the car license plates it was trained on a [data set from Kaggle](https://www.kaggle.com/datasets/andrewmvd/car-plate-detection).  
                     The model reached outstanding performance after 20 epochs of training. Some of the performance metrics measured on the validation subset are presented in the figure below.
                     ''')
@@ -194,11 +196,21 @@ def imageInput(device, src):
         st.image(fig02, caption='Model performance') 
         st.markdown(''' 
                      ### Image processing step
-                     Optical character recognition can be improved if images of the detected licence plates are preprocessed to remove noise and unwanted feature which can be misrecognized as a character. This preprocessing step includes several operation such as converting image to a binary form, contour detection, erosion, dilation, etc. After that, prepared image is supplied to the OCR engine.
+                     After detection of the license plate the image is cropped around it. 
+                     Before sending this croped image to OCR the image preprocessing step 
+                     is applied. The image preprocessing is realized using [openCV library](https://opencv.org/) and
+                     includes several operation such as removing certain colors, thresholding, 
+                     filtering, contour detection and clearing unsuitable ones. There are several 
+                     oreprocessors realized withing this project. If OCR result achieved after
+                     one preprocessor doesn't match certain criteria, another preprocessor is used
+                     and the OCR step is repeated'
                      
                      ### Optical character recognition
-                     To recognized text on the license plate tesseract [Tesseract Open Source OCR Engine](https://github.com/tesseract-ocr/tesseract) and a python library [pytesseract](https://pypi.org/project/pytesseract/) was used. The result of OCR is automatically analysed and, if necessary, other image preprocessing parameters or other parameters of the OCR engine ware used
-                     ''')      
+                     To recognized text on the license plate [Tesseract Open Source OCR Engine](https://github.com/tesseract-ocr/tesseract) 
+                     and a python library [pytesseract](https://pypi.org/project/pytesseract/) was used. 
+                     The result of OCR is automatically analysed and, if necessary, another image 
+                     preprocessor or other parameters of the OCR engine are used
+                     ''')
 
 def main():
     # -- Sidebar
